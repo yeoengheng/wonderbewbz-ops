0c307fca577e650d0005943130c91380d9a29b32
Eng Heng <enghengyeo12@Hotmail.com>
2025-09-11 20:02:50 +0800

    Make / private

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 // Define public routes that don't require authentication
-const isPublicRoute = createRouteMatcher(["/", "/auth/v2/login", "/auth/v2/register"]);
+const isPublicRoute = createRouteMatcher(["/auth/v2/login", "/auth/v2/register"]);
 
 // Define auth routes
 const isAuthRoute = createRouteMatcher(["/auth/v2/login", "/auth/v2/register"]);
